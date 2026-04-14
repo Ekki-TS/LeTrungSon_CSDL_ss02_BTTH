decab87add7e38453fd5ac275c4ef67582f022f8
Ekki-TS <trungsonle32@gmail.com>
2026-04-14 23:24:27 +0700

    Update btth.sql

--- btth.sql
+++ btth.sql
@@ -4,18 +4,20 @@ DROP TABLE IF EXISTS BOOKS;
 CREATE TABLE BOOKS (
     book_id CHAR(5) PRIMARY KEY, 
     book_name VARCHAR(200) NOT NULL, 
-    book_quantity INT CHECK (book_quantity >= 0),
-    book_rent DECIMAL (10,2) DEFAULT 5000.00
+    book_quantity INT,
+    book_rent DECIMAL (10,2) DEFAULT 5000.00,
+    input_date DATE,
+    
+    CHECK (book_quantity >= 0)
 );
 
-ALTER TABLE BOOKS ADD input_date DATE;
-
 CREATE TABLE BORROW_BOOKS (
-    br_code INT NOT NULL AUTO_INCREMENT, 
+    br_code INT AUTO_INCREMENT PRIMARY KEY, 
     book_id CHAR(5) NOT NULL, 
     NgayMuon DATE,
-    PRIMARY KEY (br_code),
-    CONSTRAINT FK_Borrow_Books FOREIGN KEY (book_id) REFERENCES BOOKS (book_id)
+    
+    CONSTRAINT FK_Borrow_Books 
+    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
 );
 
 SELECT * FROM BOOKS; 
